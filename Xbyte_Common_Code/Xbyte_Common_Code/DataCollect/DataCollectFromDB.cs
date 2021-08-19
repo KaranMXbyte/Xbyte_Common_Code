70d97fedceb6be684edbe95992b6aad925969022
KaranMXbyte <karan.mehta.xbyte@gmail.com>
2021-08-19 17:52:19 +0530

    added testting window app

--- Xbyte_Common_Code/Xbyte_Common_Code/DataCollect/DataCollectFromDB.cs
+++ Xbyte_Common_Code/Xbyte_Common_Code/DataCollect/DataCollectFromDB.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xbyte_Common_Code.Models;
 
 namespace Xbyte_Common_Code.DataCollect
 {
     class DataCollectFromDB
     {
+        #region Variables
+
+        List<P_And_G_Model> p_And_G_Models;
+        #endregion
+
+
+        #region Constructore
+
+        public DataCollectFromDB()
+        {
+            p_And_G_Models = new List<P_And_G_Model>();
+        } 
+        #endregion
+
+        public List<P_And_G_Model> GetP_And_G_DataFromDb()
+        {
+
+
+            return p_And_G_Models;
+        }
     }
 }
